afe8417c2036048145eddafe9f10cefe36ce96e3
Glitch (summer-successful-plaster) <none>
2020-12-09 22:23:38 +0000

    🏮🏌 Checkpoint ./commands/search.js:13241842/40 ./commands/playlist.js:13241842/14 ./commands/invite.js:13241842/2 ./commands/loop.js:13241842/2 ./commands/move.js:13241842/2 ./commands/nowplaying.js:13241842/2 ./commands/pause.js:13241842/2 ./commands/ping.js:13241842/2 ./commands/play.js:13241842/2 ./commands/pruning.js:13241842/2

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "ping",
   cooldown: 10,
-  description: "Show the bot's average ping",
+  description: "☾Show the bot's average ping☽",
   execute(message) {
     message.reply(`📈 Average ping to API: ${Math.round(message.client.ws.ping)} ms`).catch(console.error);
   }
