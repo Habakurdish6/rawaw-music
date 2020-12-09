5a92671a76306461f586c7fc0bee9377c4c7715d
Glitch (summer-successful-plaster) <none>
2020-12-09 21:49:24 +0000

    👑🌥 Checkpoint ./index.js:13241842/66 ./commands/clip.js:13241842/14 ./commands/clips.js:13241842/2

--- commands/clips.js
+++ commands/clips.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 module.exports = {
   name: "clips",
-  description: "List all clips",
+  description: "☾List all clips☽",
   execute(message) {
     fs.readdir("./sounds", function(err, files) {
       if (err) return console.log("Unable to read directory: " + err);
