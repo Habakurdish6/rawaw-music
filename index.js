6a081e61878dada4503781541096b73eb8ac7fd6
Glitch (summer-successful-plaster) <none>
2020-12-17 19:21:53 +0000

    💅🎑 Checkpoint ./config.json.example:22136672/100 ./index.js:26393892/118 ./config.json:22136672/3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { TOKEN, PREFIX } = require("./util/EvobotUtil");
 
 const client = new Client({ disableMentions: "everyone" });
 
-client.login("NzgyODkxNTEyODI3NDc4MDQ2.X8Syig.P1Sb_3EU0z9UpRc-UwsGAyPNSL4");
+client.login("Nzg5MTg0MzQ4MzcyNDY3NzMy.X9uXMg.bT5u_dl8XPQwg24w24vyEinTnM0");
 client.commands = new Collection();
 client.prefix = PREFIX;("a!")
 client.queue = new Map();
